9d1a935af37e96fe630d56aa1b6187354011fe5f
bea-bp <129992845+bea-bp@users.noreply.github.com>
2023-07-04 16:56:48 +0000

    my first data modeling

--- src/models.py
+++ src/models.py
@@ -1,29 +1,50 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(100), nullable=False)
+    weight = Column(Integer)
+    color_eyes  = Column (String (25))
+    born_date = Column(Integer)
+    height = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    name= Column(String(100), nullable=False)
+    diameter = Column(Integer)
+    population = Column(Integer)
+
+class Ships(Base):
+    __tablename__ = 'ships'
+    id = Column(Integer, primary_key=True)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    name= Column(String(100))
+    model= Column(String(100)) 
+    crew = Column(Integer)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(Planets)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    ship_id = Column(Integer, ForeignKey('ships.id'))
+    ships = relationship(Ships)
+
 
     def to_dict(self):
         return {}
